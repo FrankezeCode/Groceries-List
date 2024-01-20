75bb9b16f7ec98a1528e4e306cfa09d4793f5f6a
franklyn-ezeugonna <franklynezeugonna@gmail.com>
2024-01-20 05:46:37 -0800

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+
+//  const headerStyle = {
+//     backgroundColor: 'mediumseagreen',
+//     color: ''
+// }
+
+  return (
+    <header>
+        <h1>
+            Groceries List
+        </h1>
+    </header>
+  )
+}
+
+export default Header
